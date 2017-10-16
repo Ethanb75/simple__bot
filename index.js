952638a0394d5ac8a63aae87f7f2b3777457a9e5
Ethan Bellora <Belloramail@gmail.com>
2017-10-16 14:35:41 -0400

    added procfile

--- index.js
+++ index.js
@@ -1,32 +1,64 @@
-var Twitter = require('twitter');
+var Twit = require('twit');
 var config = require('./config.js');
-var meh = new Twitter(config);
+var client = new Twit(config);
 
-(function() {
-  var params = {
-    q: '#nodejs',
-    count: 10,
-    result_type: 'recent',
-    lang: 'en'
-  }
-  
-  
-  
-  meh.get('search/tweets', params, function(err, data, response) {
-    if(!err){
-      // console.log(data)
-    } else {
-      console.log(err);
-    }
-  });
-  
-  setInterval(function() {
-    meh.post('statuses/update', {status: 'I Love Twitter'})
-    .then(function (tweet) {
-      console.log(tweet);
-    })
-    .catch(function (error) {
-      throw error;
-    })
-  }, 10000);
-}())
+
+//also can do track: ['bananas', 'oranges', 'strawberries']
+// var stream = client.stream('statuses/filter', {track: 'Development'});
+var userStream = client.stream('user');
+
+
+// stream.on('tweet', function (tweet) {
+//  console.log(tweet.text)
+// });
+
+userStream.on('follow', function(msg) {
+  console.log(msg.source.screen_name);
+  client.post('statuses/update', { status: `Thanks for the follow ${msg.source.screen_name}` }, function(err, data, response) {
+    console.log(data)
+  })
+})
+
+// userStream.on('unfollow', function(msg) {
+//   console.log(msg);
+// })
+
+// T.get('followers/ids', { screen_name: 'tolga_tezel' },  function (err, data, response) {
+//   console.log(data)
+// })
+
+// post a tweet with media
+//
+// var b64content = fs.readFileSync('/path/to/img', { encoding: 'base64' })
+
+// // first we must post the media to Twitter
+// T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+//   // now we can assign alt text to the media, for use by screen readers and
+//   // other text-based presentations and interpreters
+//   var mediaIdStr = data.media_id_string
+//   var altText = "Small flowers in a planter on a sunny balcony, blossoming."
+//   var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+//   T.post('media/metadata/create', meta_params, function (err, data, response) {
+//     if (!err) {
+//       // now we can reference the media and post a tweet (media will attach to the tweet)
+//       var params = { status: 'loving life #nofilter', media_ids: [mediaIdStr] }
+
+//       T.post('statuses/update', params, function (err, data, response) {
+//         console.log(data)
+//       })
+//     }
+//   })
+// })
+
+
+
+//Based on Location
+
+// var sanFrancisco = [ '-122.75', '36.8', '-121.75', '37.8' ]
+
+// var stream = T.stream('statuses/filter', { locations: sanFrancisco })
+
+// stream.on('tweet', function (tweet) {
+//   console.log(tweet)
+// })
