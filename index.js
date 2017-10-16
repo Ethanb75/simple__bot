631787c45bbf7f44af58284355d08d74335b5724
Ethan Bellora <Belloramail@gmail.com>
2017-10-16 14:53:41 -0400

    added procfile

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var Twit = require('twit');
-var config = require('./config.js');
+var config = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+}
 var client = new Twit(config);
 
 
