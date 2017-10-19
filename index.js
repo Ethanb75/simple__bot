b9054c8c54798600549810a341d5b42c45355153
Ethan Bellora <Belloramail@gmail.com>
2017-10-19 13:43:08 -0400

    modified time for bot

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var Twit = require('twit');
-// var config = {
-//   consumer_key: process.env.CONSUMER_KEY,
-//   consumer_secret: process.env.CONSUMER_SECRET,
-//   access_token: process.env.ACCESS_TOKEN,
-//   access_token_secret: process.env.ACCESS_TOKEN_SECRET
-// }
-var config = require('./config');
+var config = {
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+}
+// var config = require('./config');
 var client = new Twit(config);
 var oldFollowers = 0;
 
